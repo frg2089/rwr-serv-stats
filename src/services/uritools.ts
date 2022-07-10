51e5c185938b450f578860da1332928d13bfd16d
舰队的偶像-岛风酱! <frg2089@outlook.com>
2022-07-10 18:43:04 +0800

    :bug: 修steam uri 抄都能抄歪来

--- src/services/uritools.ts
+++ src/services/uritools.ts
@@ -14,5 +14,5 @@ export const getSteamUri = (addr: string) => {
   if (split.length > 1) {
     port = split[1]
   }
-  return `steam://rungameid/270150/server_address=${address} server_port=${port}`
+  return `steam://rungameid/270150//server_address=${address} server_port=${port}`
 }
