e390ae5e6dcbfbdf261f67a8e8c8cfab73b1aed2
舰队的偶像-岛风酱! <frg2089@outlook.com>
2023-06-14 09:46:09 +0800

    :bug: 修复按钮不能被点击的错误
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/components/RWRButton.vue
+++ src/components/RWRButton.vue
@@ -1,9 +1,10 @@
 <template>
 <component
     :is="href ? 'a' : 'button'"
+    :href="href" target="_blank"
     :border="`.1rem solid ${accent ? 'primary-light' : 'neutral-thertiary-alt'} hover:primary-light`"
-    p="x-[.8rem] y-[.3rem]" :bg="accent ? 'primary' : 'neutral-thertiary'" font-bold text-white
-    hover:bg-primary
+    p="x-[.8rem] y-[.3rem]" :bg="accent ? 'primary' : 'neutral-thertiary'"
+    font-bold text-white decoration-none hover:bg-primary
     @click="$emit('click', $event)"
 >
   <slot />
