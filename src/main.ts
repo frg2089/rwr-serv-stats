750d67798431d287b328648cb34f868ac585318d
舰队的偶像-岛风酱! <frg2089@outlook.com>
2024-02-21 07:44:05 +0800

    Update
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,4 @@ import 'virtual:uno.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-if (import.meta.env.DEV) {
-  console.log(`RWR_SERVER_LIST: ${import.meta.env.RWR_SERVER_LIST} RWR_NOTICE_URI: ${import.meta.env.RWR_NOTICE_URI} RWR_ADVANCED_INFO_URI: ${import.meta.env.RWR_ADVANCED_INFO_URI ?? ''}`)
-}
-
 createApp(App).mount('#app')
